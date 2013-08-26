c6ea8cc0128e2c0a4ee09f81323552525b0b0b73
Vamsee Kanakala <vamsee@riseup.net>
2013-08-26 17:24:46 +0530

    add vagrant config

--- manifests/init.pp
+++ manifests/init.pp
@@ -33,81 +33,83 @@ class solr ($cores = ['development','test']) {
   
   $jetty_home = "/usr/share/jetty"
   $solr_home = "/usr/share/solr"
-  
+
+  package { 'default-jdk':
+    ensure => present,
+  }
+ 
   package { 'solr-jetty':
     ensure => present,
+    require => Package['default-jdk'],
   }
 
-  package { 'openjdk-6-jdk':
-    ensure => present,
-  }
 
-  #Removes existing solr install
-  exec { 'rm-web-inf':
-    command => "rm -rf ${solr_home}/WEB-INF",
-    path => ["/usr/bin", "/usr/sbin", "/bin"],
-    onlyif => "test -d ${solr_home}/WEB-INF",
-    require => Package['solr-jetty'],
-  }
-
-  #Removes existing solr config
-  exec { 'rm-default-conf':
-    command => "rm -rf ${solr_home}/conf",
-    path => ["/usr/bin", "/usr/sbin", "/bin"],
-    onlyif => "test -d ${solr_home}/conf",
-    require => Exec['rm-web-inf'],
-  }
-
-  #Removes existing solr webapp in jetty
-  exec { 'rm-solr-link':
-    command => "rm -rf ${jetty_home}/webapps/solr",
-    path => ["/usr/bin", "/usr/sbin", "/bin"],
-    onlyif => "test -L ${jetty_home}/webapps/solr",
-    require => Exec['rm-default-conf'],
-  }
-
-  #Replaces with our newer version. You can download the
-  #latest version and add it if you need latest features. 
-  file { 'solr.war':
-    ensure => file,
-    path => "${jetty_home}/webapps/solr.war",
-    source => "puppet:///modules/solr/solr.war",
-    require => Exec['rm-solr-link'],
-  }
-
-  #Add a new solr context to jetty
-  file { 'jetty-context.xml':
-    ensure => file,
-    path => "${jetty_home}/contexts/solr.xml",
-    source => "puppet:///modules/solr/jetty-context.xml",
-    require => File['solr.war'],
-  }
-
-  #Copy the jetty config file
-  file { 'jetty-default':
-    ensure => file,
-    path => "/etc/default/jetty",
-    source => "puppet:///modules/solr/jetty-default",
-    require => File['jetty-context.xml']
-  }
-
-  #Copy the solr config file
-  file { 'solr.xml':
-    ensure => file,
-    path => "${solr_home}/solr.xml",
-    content => template('solr/solr.xml.erb'),
-    require => File['jetty-default'],
-  }
-
-  #Restart after copying new config
-  service { 'jetty':
-    ensure => running,
-    hasrestart => true,
-    hasstatus => true,
-    subscribe => File['solr.xml'],
-  }
-
-  #Create our solr cores
-  solr::core { $cores: }
-  
+#   #Removes existing solr install
+#   exec { 'rm-web-inf':
+#     command => "rm -rf ${solr_home}/WEB-INF",
+#     path => ["/usr/bin", "/usr/sbin", "/bin"],
+#     onlyif => "test -d ${solr_home}/WEB-INF",
+#     require => Package['solr-jetty'],
+#   }
+# 
+#   #Removes existing solr config
+#   exec { 'rm-default-conf':
+#     command => "rm -rf ${solr_home}/conf",
+#     path => ["/usr/bin", "/usr/sbin", "/bin"],
+#     onlyif => "test -d ${solr_home}/conf",
+#     require => Exec['rm-web-inf'],
+#   }
+# 
+#   #Removes existing solr webapp in jetty
+#   exec { 'rm-solr-link':
+#     command => "rm -rf ${jetty_home}/webapps/solr",
+#     path => ["/usr/bin", "/usr/sbin", "/bin"],
+#     onlyif => "test -L ${jetty_home}/webapps/solr",
+#     require => Exec['rm-default-conf'],
+#   }
+# 
+#   #Replaces with our newer version. You can download the
+#   #latest version and add it if you need latest features. 
+#   file { 'solr.war':
+#     ensure => file,
+#     path => "${jetty_home}/webapps/solr.war",
+#     source => "puppet:///modules/solr/solr.war",
+#     require => Exec['rm-solr-link'],
+#   }
+# 
+#   #Add a new solr context to jetty
+#   file { 'jetty-context.xml':
+#     ensure => file,
+#     path => "${jetty_home}/contexts/solr.xml",
+#     source => "puppet:///modules/solr/jetty-context.xml",
+#     require => File['solr.war'],
+#   }
+# 
+#   #Copy the jetty config file
+#   file { 'jetty-default':
+#     ensure => file,
+#     path => "/etc/default/jetty",
+#     source => "puppet:///modules/solr/jetty-default",
+#     require => File['jetty-context.xml']
+#   }
+# 
+#   #Copy the solr config file
+#   file { 'solr.xml':
+#     ensure => file,
+#     path => "${solr_home}/solr.xml",
+#     content => template('solr/solr.xml.erb'),
+#     require => File['jetty-default'],
+#   }
+# 
+#   #Restart after copying new config
+#   service { 'jetty':
+#     ensure => running,
+#     hasrestart => true,
+#     hasstatus => true,
+#     subscribe => File['solr.xml'],
+#   }
+# 
+#   #Create our solr cores
+#   solr::core { $cores: }
+#   
 }
---
+++ vagrant/base.pp
@@ -0,0 +1,22 @@
+# the default path for puppet to look for executables
+Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
+
+stage { 'first': }
+
+stage { 'last': }
+
+Stage['first'] -> Stage['main'] -> Stage['last']
+
+class update_aptget {
+  exec {"apt-get update && touch /tmp/apt-get-updated":
+    unless => "test -e /tmp/apt-get-updated"
+  }
+}
+ 
+# run apt-get update before anything else runs
+class {'update_aptget':
+  stage => first,
+}
+
+class {'solr': }
+
