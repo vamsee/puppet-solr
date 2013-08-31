0b0a338799321796143163b365bb891945635445
Vamsee Kanakala <vamsee@riseup.net>
2013-08-31 18:49:52 +0530

    add jetty-default spec

--- spec/classes/config_spec.rb
+++ spec/classes/config_spec.rb
@@ -2,15 +2,20 @@ require 'spec_helper'
 
 describe 'solr::config' do
 
-  it do
-    should contain_file('solr').with({
+  it { should contain_file('jetty-default').with({
+    'ensure'    =>    'file',
+    'path'      =>    '/etc/default/jetty',
+    'source'    =>    'puppet:///modules/solr/jetty-default'})
+    .with_require('Package[jetty]')
+  }
+
+  it { should contain_file('solr').with({
       'ensure'  => 'directory',
       'owner'   => 'root',
       'group'   => 'root',
       'recurse' => 'true',
       'path'    => '/usr/share/solr',
       'source'  => 'puppet:///modules/solr/solr',
-    })
-  end
+    })}
 
 end
