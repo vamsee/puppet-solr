1f53b71e065388e94cada3517034042e8198c86e
Tom Murphy <t.murphy@ctidigital.com>
2017-03-02 10:43:11 +0000

    Set $jetty_home properly depending on version of jetty

--- manifests/params.pp
+++ manifests/params.pp
@@ -16,16 +16,19 @@ class solr::params {
 
   case $::lsbdistcodename {
     'precise': {
+      $jetty_home    = '/usr/share/jetty'
       $jetty_package = 'jetty'
       $jdk_dirs = '/usr/lib/jvm/default-java /usr/lib/jvm/java-6-sun'
     }
 
     'trusty': {
+      $jetty_home    = '/usr/share/jetty'
       $jetty_package = 'jetty'
       $jdk_dirs = '/usr/lib/jvm/default-java /usr/lib/jvm/java-7-openjdk-amd64'
     }
 
     default: {
+      $jetty_home    = '/usr/share/jetty8'
       $jetty_package = 'jetty8'
       $jdk_dirs = '/usr/lib/jvm/default-java /usr/lib/jvm/java-7-openjdk-amd64'
     }
