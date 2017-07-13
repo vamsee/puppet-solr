289757979da675935389fd6a9483d7aefbcf9799
Tom Murphy <tdm4@users.noreply.github.com>
2017-07-13 12:02:24 +0100

    Extend timeout for Java 8 install form 5 minutes to 15 minutes

--- manifests/install.pp
+++ manifests/install.pp
@@ -82,6 +82,7 @@ class solr::install {
     exec { 'install-java8-for-solr':
       command => "add-apt-repository -y ppa:webupd8team/java; apt-get -y update; echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections; apt-get -y install oracle-java8-installer",
       require => Package['python-software-properties', 'software-properties-common'],
+      timeout => 900,
       creates => "/usr/lib/jvm/java-8-oracle"
     }
   }
